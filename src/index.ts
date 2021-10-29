283d9af8ec616acda02c0a9c708bd09d6ece1297
depthbomb <reesepup77@hotmail.com>
2021-10-28 19:24:13 -0500

    Initial

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { URL } from 'node:url';
+
+/**
+ * Capitalizes the first letter of a string
+ * @param {string} string Input string
+ * @returns {string} The input string with the first letter capitalized
+ */
+export function capitalize(string: string): string {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+/**
+ * Checks if a URL is valid
+ * @param {string} url URL to check
+ * @returns {boolean} True if the provided URL is valid
+ */
+export function isValidURL(string: string): boolean {
+	try {
+		new URL(string);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
